5e159446e2c2644c78b9bbbef0ea02c49437769e
ghostgirl127 <telegatto01@gmail.com>
2024-11-26 11:52:26 +0100

    Update inputReminderLogic.js

--- public/js/eventListeners/inputReminderLogic.js
+++ public/js/eventListeners/inputReminderLogic.js
@@ -2,13 +2,23 @@ const input21 = document.querySelector("#last21")
 const input22 = document.querySelector("#last22")
 const input23 = document.querySelector("#last23")
 const input24 = document.querySelector("#last24")
-input21.addEventListener('input', function() {
+
+function aggiornaValore(id) {
     // Ottieni il valore dell'input
-    var numero = input21.value;
-    console.log(numero)
+
+    let numero;
+
+    if (id == "21")
+        numero = input21.value;
+    if (id == "22")
+        numero = input22.value;
+    if (id == "23")
+        numero = input23.value;
+    if (id == "24")
+        numero = input24.value;
 
     // Invia il dato al server usando fetch
-    fetch('/aggiorna-valore21', {
+    fetch(`/aggiorna-valore/${id}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json' // Specifica il tipo di contenuto
@@ -17,80 +27,20 @@ input21.addEventListener('input', function() {
             valore: numero // Invia il valore come JSON
         })
     })
-    .then(response => response.json())  // Gestisci la risposta
-    .then(data => {
-        console.log('Risposta del server:', data);
-    })
     .catch(error => {
         console.error('Errore:', error);
     });
+}
+
+input21.addEventListener('input', function() {
+    aggiornaValore("21")
 });
 input22.addEventListener('input', function() {
-    // Ottieni il valore dell'input
-    var numero = input22.value;
-    console.log(numero)
-
-    // Invia il dato al server usando fetch
-    fetch('/aggiorna-valore22', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json' // Specifica il tipo di contenuto
-        },
-        body: JSON.stringify({
-            valore: numero // Invia il valore come JSON
-        })
-    })
-    .then(response => response.json())  // Gestisci la risposta
-    .then(data => {
-        console.log('Risposta del server:', data);
-    })
-    .catch(error => {
-        console.error('Errore:', error);
-    });
+    aggiornaValore("22")
 });
 input23.addEventListener('input', function() {
-    // Ottieni il valore dell'input
-    var numero = input23.value;
-    console.log(numero)
-
-    // Invia il dato al server usando fetch
-    fetch('/aggiorna-valore23', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json' // Specifica il tipo di contenuto
-        },
-        body: JSON.stringify({
-            valore: numero // Invia il valore come JSON
-        })
-    })
-    .then(response => response.json())  // Gestisci la risposta
-    .then(data => {
-        console.log('Risposta del server:', data);
-    })
-    .catch(error => {
-        console.error('Errore:', error);
-    });
+    aggiornaValore("23")
 });
 input24.addEventListener('input', function() {
-    // Ottieni il valore dell'input
-    var numero = input24.value;
-    console.log(numero)
-
-    // Invia il dato al server usando fetch
-    fetch('/aggiorna-valore24', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json' // Specifica il tipo di contenuto
-        },
-        body: JSON.stringify({
-            valore: numero // Invia il valore come JSON
-        })
-    })
-    .then(response => response.json())  // Gestisci la risposta
-    .then(data => {
-        console.log('Risposta del server:', data);
-    })
-    .catch(error => {
-        console.error('Errore:', error);
-    });
+    aggiornaValore("24")
 });
